d0112bc1ad27a63caaaab7304f7e2d40a7556596
fedeloxe <loxeee@gmail.com>
2022-12-15 20:00:15 +0100

    milestone 2 done

--- js/main.js
+++ js/main.js
@@ -55,3 +55,45 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+//-----------Stampa post su html-----------------------------
+let post
+
+posts.forEach(element =>{post += 
+    `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="immagine-profilo">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${element.content}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
+
+});
+
+const textPost = document.getElementById('container');
+textPost.innerHTML += post
